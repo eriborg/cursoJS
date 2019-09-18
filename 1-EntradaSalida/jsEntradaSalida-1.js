fb794f55124ddd0da3fd28809968f915903dda58
alumno <alumno@utnfra.com.ar>
2019-09-18 16:53:39 -0300

    clase 3

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,4 +1,13 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
+/*
+var boton;
+window.addEventListener('load', inicializarManejadores);
+
+function inicializarManejadores(){
+	boton = document.getElementById('btnMostrar');
+	console.log(boton);
+}
+*/
 function mostrar()
 {
 	alert("Esto funciona de maravilla");
--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,31 @@
 function mostrar()
 {
-
-	var contador=0;
+	var contador = 0;
 	var acumulador=0;
+	var numero;
 
+	/*
+	Utilizando el FOR
+	for (let i = 0; i < 5; i++) {
+		
+		numero = parseInt(prompt("Ingrese un numero "));
+		acumulador += numero; 		
+		
+	}
+	*/
 
+	/*
+	Utilizando el while
+	*/
+	
+	while (contador < 5) {
+		numero = parseInt(prompt("Ingrese un numero "));
+		acumulador += numero; 
+		contador++;
+	}
+	
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+	document.getElementById('suma').value = acumulador;
+	document.getElementById('promedio').value = acumulador/contador;
 
 }//FIN DE LA FUNCIÓN
