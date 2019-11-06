901dcd1428d5e5d1e313577e47b08f52fa73cedb
eriborg <erica.borgeat@gmailcom>
2019-11-06 00:35:37 -0300

    Ejercicios de filter

--- mapReduceFilter/Consultas/scripts.js
+++ mapReduceFilter/Consultas/scripts.js
@@ -4,70 +4,71 @@
     realizar las operaciones usando los metodos map,  reduce y filter y combinaciones entre ellos
   */
 
+ // Retornar un array con los nombres de los usuarios femeninos
 
-var soluciones = {};
+ let usuariosFemeninos = data.filter(p => p.genero === 'Female').map(p => p.nombre);
+ //console.log(usuariosFemeninos);
+ 
+ // Retornar un array de strings (el email de los usuarios de sexo masculino)
+ let emailVarones = data
+                    .filter(function(usuario) {
+                        return usuario.genero === 'Male'; 
+                    })
+                    .map(function(usuario) {
+                        return usuario.email;
+                    });
 
-// Retornar un array con los nombres de los usuarios femeninos
+//console.log(emailVarones);
 
-soluciones.usuariosFemeninos = function(usuarios){
-    return usuarios
-    .filter(function(user){
-        return user.genero === 'Female';
-    })
-    .map(function(user){
-        return user.nombre;
-    });
-}
+let emailVarones2 = data.filter(p => p.genero === 'Male').map(p => p.email);
+console.log(emailVarones2);
+ 
+ // Retornar un array de objetos que solo contengan las claves nombre, email y edad, de todos los usuarios mayores que 'edad'
 
-//console.log(soluciones.usuariosFemeninos(data));
+ let edad = 40;
+ let usuario = data;
 
-// Retornar un array de strings (el email de los usuarios de sexo masculino)
+ //let usuarioMayor = data.filter(p => p.edad > edad).map(d => {"nombre": d.nombre, "genero": d.genero});
+ 
+ let usuarioMasGrande = data
+                    .filter(function(arr) {
+                        return arr.edad > edad;
+                    })
+                    .map(function(a) {
+                        return {nombre: a.nombre, genero: a.email, edad: a.edad};
+                    });
 
-soluciones.mailsVarones = function(usuarios){
+ console.log(usuarioMasGrande);
+
+ // Retornar un objeto que contenga solo el nombre y la edad del usuario mas grande.
+ 
+ 
+ 
+ //console.log(soluciones.usuarioMasGrande(data));
+ 
+ // Retornar el promedio de edad de los usuarios (number)
+ 
+ soluciones.promedio = function(usuarios){
    
-}
-
-//console.log(soluciones.mailsVarones(data));
-
-// Retornar un array de objetos que solo contengan las claves nombre, email y edad, de todos los usuarios mayores que 'edad'
-
-soluciones.usuariosMayores = function(usuarios, edad){
+ 
+     return (acumEdad /cantidad).toFixed(2);
+ }
+ 
+ //console.log("Promedio edad usuarios " + soluciones.promedio(data));
+ 
+ // Retornar el promedio de edad de los usuarios hombres (number)
+ 
+ soluciones.promedioVarones = function(usuarios){
     
-}
-
-//console.log(soluciones.usuariosMayores(data, 40));
-
-  // Retornar un objeto que contenga solo el nombre y la edad del usuario mas grande.
-
-soluciones.usuarioMasGrande = function(usuarios){
     
-}
-
-//console.log(soluciones.usuarioMasGrande(data));
-
-// Retornar el promedio de edad de los usuarios (number)
-
-soluciones.promedio = function(usuarios){
-  
-
-    return (acumEdad /cantidad).toFixed(2);
-}
-
-//console.log("Promedio edad usuarios " + soluciones.promedio(data));
-
-// Retornar el promedio de edad de los usuarios hombres (number)
-
-soluciones.promedioVarones = function(usuarios){
-   
-   
-}
-
-//console.log("Promedio edad Varones " + soluciones.promedioVarones(data));
-
- // Retornar el promedio de edad de los usuarios mujeres (number)
-
-soluciones.promedioMujeres = function(usuarios){
-   
-}
-
-//console.log("Promedio edad Mujeres " + soluciones.promedioMujeres(data));
+ }
+ 
+ //console.log("Promedio edad Varones " + soluciones.promedioVarones(data));
+ 
+  // Retornar el promedio de edad de los usuarios mujeres (number)
+ 
+ soluciones.promedioMujeres = function(usuarios){
+    
+ }
+ 
+ //console.log("Promedio edad Mujeres " + soluciones.promedioMujeres(data));
